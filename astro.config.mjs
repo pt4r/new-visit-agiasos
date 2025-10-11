b2086c5eded29d4e44cb810aa7057ad9986bdc3a
Pt4r <palaiologosstr@hotmail.com>
2025-10-12 02:55:34 +0300

    re add them

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   site: "https://new-visitagiasos.netlify.app",
-  trailingSlash: 'never',
   integrations: [
     icon(),
     i18n({
